29c63448003d5ffc5a9f4d9a5824d6bbd609c6a4
ExoOmega <Roxsisshinn@gmail.com>
2018-03-28 22:31:01 -0400

    Working Select and Move Prototype
    https://www.youtube.com/watch?v=hKE8bqaGspM&list=PL1bPKmY0c-wn5J-hoUWTjZFAk8XkoZ9hl

---
+++ Unity/mc2redux/Assets/Code/CharacterStats.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public float health;
+    public bool selected;
+    public bool dead;
+    public int team;
+    public bool crouch;
+    public bool run;
+    public GameObject selectCude;
+    PlayerControl plControl;
+
+	// Use this for initialization
+	void Start ()
+    {
+        plControl = GetComponent<PlayerControl>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        selectCude.SetActive(selected);
+
+        if (run)
+        {
+            crouch = false;
+        }
+	}
+
+    public void MoveToPosition(Vector3 position)
+    {
+        plControl.moveToPosition = true;
+        plControl.destPosition = position;
+    }
+}
